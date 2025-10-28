26db78b06d330ebaf205e82aadef5196f82bb290
Kateryna Porva <lithabeta80@gmail.com>
2025-10-28 14:37:52 +0200

    Add new unit tests for UserService

---
+++ MiniBlogApp.Tests/tests/UserServiceAdvancedTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using MiniBlogApp.Services;
+using Xunit;
+
+namespace MiniBlogApp.Tests.ServiceTests
+{
+    public class UserServiceAdvancedTests
+    {
+        private readonly UserService _service = new();
+
+        [Fact]
+        public void Authenticate_ShouldBeCaseSensitive()
+        {
+            var result = _service.Authenticate("Serhii", "1234");
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void Authenticate_ShouldFailForEmptyPassword()
+        {
+            var result = _service.Authenticate("serhii", "");
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void GetAll_ShouldReturnAtLeastTwoUsers()
+        {
+            Assert.True(_service.GetAll().Count() >= 2);
+        }
+    }
+}
