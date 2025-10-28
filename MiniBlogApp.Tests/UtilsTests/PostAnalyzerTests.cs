d46a8c30da276b4e0fb09e6ef51e5c650f008023
Kateryna Porva <lithabeta80@gmail.com>
2025-10-28 14:33:43 +0200

    Improve PostAnalyzerTests

--- MiniBlogApp.Tests/UtilsTests/PostAnalyzerTests.cs
+++ MiniBlogApp.Tests/UtilsTests/PostAnalyzerTests.cs
@@ -1,6 +1,7 @@
 ﻿using MiniBlogApp.Models;
 using MiniBlogApp.Services;
 using Xunit;
+using System.Collections.Generic;
 
 namespace MiniBlogApp.Tests.UtilsTests
 {
@@ -9,7 +10,6 @@ namespace MiniBlogApp.Tests.UtilsTests
         [Fact]
         public void Analyze_ShouldReturnCorrectSummary()
         {
-            // Arrange
             var post = new Post
             {
                 Id = 1,
@@ -22,14 +22,83 @@ namespace MiniBlogApp.Tests.UtilsTests
 
             var analyzer = new PostAnalyzer<Post>();
 
-            // Act
             var result = analyzer.Analyze(post);
 
-            // Assert
             Assert.Equal(
                 "Пост 'Test Post' створений користувачем author1, має 2 лайків і 1 коментарів.",
                 result
             );
+        }
+
+        [Fact]
+        public void Analyze_ShouldHandlePostWithNoLikesOrComments()
+        {
+            var post = new Post
+            {
+                Id = 2,
+                Author = "author2",
+                Title = "Empty Post",
+                Content = "No likes or comments",
+                Likes = new List<Like>(),
+                Comments = new List<Comment>()
+            };
+
+            var analyzer = new PostAnalyzer<Post>();
+            var result = analyzer.Analyze(post);
+
+            Assert.Contains("0 лайків", result);
+            Assert.Contains("0 коментарів", result);
+        }
+
+        [Fact]
+        public void Analyze_ShouldHandlePostWithNullAuthorOrTitle()
+        {
+            var post = new Post
+            {
+                Id = 3,
+                Author = null,
+                Title = null,
+                Content = "Content here",
+                Likes = new List<Like>(),
+                Comments = new List<Comment>()
+            };
+
+            var analyzer = new PostAnalyzer<Post>();
+            var result = analyzer.Analyze(post);
+
+            Assert.Contains("створений користувачем", result);
+            Assert.Contains("0 лайків", result);
+            Assert.Contains("0 коментарів", result);
+        }
+
+        [Fact]
+        public void Analyze_ShouldHandlePostWithManyLikesAndComments()
+        {
+            var post = new Post
+            {
+                Id = 4,
+                Author = "popularUser",
+                Title = "Viral Post",
+                Content = "Lots of engagement",
+                Likes = new List<Like>
+                {
+                    new Like { Username = "u1" },
+                    new Like { Username = "u2" },
+                    new Like { Username = "u3" },
+                    new Like { Username = "u4" }
+                },
+                Comments = new List<Comment>
+                {
+                    new Comment { Author = "c1", Text = "Great!" },
+                    new Comment { Author = "c2", Text = "Amazing!" }
+                }
+            };
+
+            var analyzer = new PostAnalyzer<Post>();
+            var result = analyzer.Analyze(post);
+
+            Assert.Contains("4 лайків", result);
+            Assert.Contains("2 коментарів", result);
         }
     }
 }
